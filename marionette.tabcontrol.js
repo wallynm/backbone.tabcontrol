d7f0a428a61f665fe0a966f096f805d703abd030
Wallysson Nunes <wally.nm@gmail.com>
2015-11-16 11:32:18 -0200

    Updated the content tab position

--- marionette.tabcontrol.js
+++ marionette.tabcontrol.js
@@ -1,86 +1,81 @@
 Backbone.Marionette.TabControl = Marionette.LayoutView.extend({
-  template    : function() { return $('<ul class="nav nav-tabs"></ul><div class="tab-content"></div>'); },
-  tagName     : 'div',
-  className   : 'tabbed-content',
+  template: function() { return $('<ul class="nav nav-tabs"></ul><div class="tab-content"></div>'); },
+  tagName: 'div',
+  className: 'tabbed-content',
 
-  liView      : '<li><a href="#<%= tabid %>" role="tab" data-toggle="tab" data-i18n="<%= tabtitle %>"></a></li>',
-  tabView     : '<div role="tabpanel" class="tab-pane" id="<%= tabid %>"></div>',
-  tabs        : [],
-  data        : {},
-  defaultTab  : 0,
-  regions     : {
-    tabRegion : '.tab-content'
+  liView: '<li><a href="#<%= tabid %>" role="tab" data-toggle="tab"><%= tabtitle %></a></li>',
+  tabView: '<div role="tabpanel" class="tab-pane" id="<%= tabid %>"></div>',
+  tabs: [],
+  data: {},
+  defaultTab: 0,
+  regions: {
+    tabRegion: '.tab-content'
   },
 
-  events : {
-    'click .nav-tabs a' : 'showTab'
+  events: {
+    'click .nav-tabs a': 'showTab'
   },
 
-
-  initialize : function() {
+  initialize: function() {
     var self = this;
 
     self.addRegion('tabRegion', '.tab-content');
 
-    self.tabRegion.on('show', function( view ){
-      window.onViewRendered( view )
+    self.tabRegion.on('show', function(view) {
+      window.onViewRendered(view);
     });
-    self.tabRegion.on('empty', function( view ){
-      window.onViewChanged( view );
+    self.tabRegion.on('empty', function(view) {
+      window.onViewChanged(view);
     });
   },
 
-  setObject : function(data)
-  {
+  setObject: function(data) {
     var self = this;
-    $.each(data, function(index,value){
+    $.each(data, function(index, value) {
       self.set(index, value);
     });
   },
 
-  set : function(key, val)
-  {
+  set: function(key, val) {
     this.data[key] = val;
   },
 
-  get : function(key)
-  {
-    if ( _.isUndefined(key) )
+  get: function(key) {
+    if (_.isUndefined(key))
       return this.data;
 
     return this.data[key];
   },
 
-  onShow : function(){
+  onShow: function() {
     var self = this;
-    _.each(this.tabs, function(tab, index){
+    _.each(this.tabs, function(tab, index) {
       $tablist = self.$el.find('.nav');
       $tabcontent = self.$el.find('.tab-content');
 
       // Renderiza o html responsavel pelas tabs
-      $tablist.append(_.template(self.liView)({tabid : tab.id, tabtitle : tab.title}));
+      $tablist.append(_.template(self.liView)({tabid: tab.id, tabtitle: tab.title}));
     });
 
     self.$el.find('.nav-tabs a').eq(self.defaultTab).click();
   },
 
-  getTabId : function(id){
-    return _.filter(this.tabs, function(item){ return item.id == id })[0];
+  getTabId: function(id) {
+    return _.filter(this.tabs, function(item) { return item.id == id; })[0];
   },
 
-  setParent : function(object)
-  {
+  setParent: function(object) {
     var self = this;
-    if(_.isUndefined(object.parent))
+    if (_.isUndefined(object.parent))
       object.parent = self;
 
-    console.warn(object)
-    console.warn(object.parent)
+    console.warn(object);
+    console.warn(object.parent);
   },
 
-  showTab : function(e){
+  showTab: function(e) {
     var self = this;
-    var el = $(e.currentTarget)
+    var el = $(e.currentTarget);
     var id = el.attr('href').replace('#', '');
     var tabObject = self.getTabId(id);
     var view = (typeof tabObject.view === 'object') ? tabObject.view : new tabObject.view(this.data);
